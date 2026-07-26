09011e49d24b78fd9e225cb69e99706829cada66
AscendedAngel <hikashimlbb@gmail.com>
2026-07-26 18:42:22 +0700

    (ref) Made User Entity Compatible with EF Core

--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -2,6 +2,7 @@ namespace Domain.Users;
 
 public sealed class User
 {
+    public UserId Id { get; init; }
     public Email Email { get; init; }
     public Username Username { get; init; }
     public Password Password { get; init; }
@@ -9,9 +10,15 @@ public sealed class User
     // Sign-Up
     public User(Email email, Username username, Password password)
     {
+        Id = new UserId(Guid.CreateVersion7());
         Email = email;
         Username = username;
         Password = password;
     }
+
+    #pragma warning disable CS8618
+    // Used for Entity Framework Core
+    private User() {}
+    #pragma warning restore CS8618
 }
 
