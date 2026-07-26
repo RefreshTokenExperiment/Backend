633d5795e294fcee1ac931758344bec971c2e3eb
AscendedAngel <hikashimlbb@gmail.com>
2026-07-26 18:37:59 +0700

    (feat) Added Verify Method for Password Hasher

--- src/Domain/IPasswordHasher.cs
+++ src/Domain/IPasswordHasher.cs
@@ -1,6 +1,9 @@
+using Domain.Users;
+
 namespace Domain;
 
 public interface IPasswordHasher
 {
     public string Hash(string password);
+    public bool Verify(string password, Password originalPassword);
 }
