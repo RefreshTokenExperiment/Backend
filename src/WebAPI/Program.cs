87f9eb8e5e4ea725179e983d214dd4ae3b29d043
AscendedAngel <hikashimlbb@gmail.com>
2026-07-30 19:24:51 +0700

    (add) Added Empty Endpoints

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,6 +1,10 @@
+using WebAPI.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapAuthEndpoints();
 
 app.Run();
